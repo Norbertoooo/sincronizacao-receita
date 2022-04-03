4bab8862a474633fea88874e80120af73735a1a0
Vítor Norberto <vithor.norberto@gmail.com>
2022-04-03 01:59:49 -0300

    Remove anotações desnecessárias

--- src/main/java/com/sicredi/sincronizao/receita/SincronizacaoReceitaApplication.java
+++ src/main/java/com/sicredi/sincronizao/receita/SincronizacaoReceitaApplication.java
@@ -1,13 +1,9 @@
 package com.sicredi.sincronizao.receita;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Slf4j
 @SpringBootApplication
-@RequiredArgsConstructor
 public class SincronizacaoReceitaApplication {
 
     public static void main(String[] args) {
